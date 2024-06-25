bade734e5b288b0a4fea9895819579bc84fa8c63
Umer Aziz Rana <60209221+umerazizrana@users.noreply.github.com>
2024-06-25 16:04:55 +0500

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "UK South"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["172.16.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["172.16.1.0/24"]
+}
